283d1da09dd6f6f632c3d42426a716f9c21e6e35
Elliottscode <elliottmccully@yahoo.com>
2021-11-23 18:32:16 +0000

    Feat(Base) Template
    Base template for react webpack and bable for development

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index.bundle.js'
+    },
+    devServer: {
+        port: 3010,
+        
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',   
+                ]
+            }
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()], 
+};
